75e285703b3e7a4f0962ffc50a6dee02e5b466d3
WinnieDave <fnc01@mail.ru>
2018-03-27 22:04:23 +0300

    Latest stable version

--- NNet/Network.cs
+++ NNet/Network.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using NNet.Layers;
 using NNet.Neurons;
 
@@ -9,10 +10,13 @@ namespace NNet
         public List<Layer> Layers { get; }=new List<Layer>();
         public double[] Output;
         private int _depth = 0;
+        private OutputLayer _outputLayer;
+        private InputLayer _inputLayer;
         public Network(int inputCapacity, int outputCapacity, params int[] hiddenCapacities)
         {
             this._depth = 2 + hiddenCapacities.Length;
             Layers.Add(new InputLayer(inputCapacity));
+            this._inputLayer = (InputLayer) Layers[0];
             for (int i = 0; i < hiddenCapacities.Length; i++)
             {
                 Layers.Add(new HiddenLayer(hiddenCapacities[i]));
@@ -23,12 +27,13 @@ namespace NNet
             {
              Layers[i-1].ConnectForwards(Layers[i]);   
             }
+            this._outputLayer = (OutputLayer) Layers[Layers.Count - 1];
             Output = new double[outputCapacity];
         }
 
         public void Train(double[]expected, double lRate, double momentum = 0.0)
         {
-            var outputLayer = Layers[Layers.Count - 1] as OutputLayer;
+            var outputLayer = _outputLayer;//Layers[Layers.Count - 1] as OutputLayer;
             outputLayer.Train(expected,lRate,.9);
             for (int i = Layers.Count - 2; i > 0; i--)
             {
@@ -39,7 +44,7 @@ namespace NNet
         public double GetMSE()
         {
             var sum=0.0;
-            var outputLayer = Layers[Layers.Count - 1] as OutputLayer;
+            var outputLayer =_outputLayer; //Layers[Layers.Count - 1] as OutputLayer;
             foreach ( var neuron in outputLayer.Neurons)
             {
                 sum += neuron.Error * neuron.Error;
@@ -48,7 +53,7 @@ namespace NNet
         }
         public void Calculate(params double[] input)
         {
-            (this.Layers[0] as InputLayer).SetInput(input);
+            _inputLayer.SetInput(input);//(this.Layers[0] as InputLayer).SetInput(input);
             foreach (var layer in Layers)
             {
                 layer.Calculate();
